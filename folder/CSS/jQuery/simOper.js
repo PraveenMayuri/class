1ee8aaf463eb016b36296a8dd4901af1056eb659
PraveenMayuri <mayuri.jayaram@gmail.com>
2024-09-15 10:11:14 -0700

    Mayuri sharing codes

---
+++ folder/CSS/jQuery/simOper.js
@@ -0,0 +1,11 @@
+let a=10;
+let b=20;
+
+console.log("The addition of the numbers is :", a+b);
+console.log("The subtraction of the numbers is :", a-b);
+console.log("The division of the numbers is :", a/b);
+console.log("The multiplication of the numbers is :", a*b);
+console.log("The modulus of the numbers is :", a%b);
+a += 5;
+console.log("The new value of a is:",a);
+
---
+++ folder/CSS/jQuery/simSyntax.js
@@ -0,0 +1,6 @@
+let name="Mayuri Praveen";
+let course="UI";
+let isStudent = true;
+console.log("My name is:"+ name );
+console.log("The name of my course is :"+ course);
+console.log("Is a student:",isStudent)
